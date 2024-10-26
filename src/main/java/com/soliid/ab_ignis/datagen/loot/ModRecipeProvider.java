df089975b01c2e27e7b17df56b7577b45ae1a244
soliid <verrico2006@gmail.com>
2024-10-25 23:19:30 -0400

    final commit before checking datagen

--- src/main/java/com/soliid/ab_ignis/datagen/loot/ModRecipeProvider.java
+++ src/main/java/com/soliid/ab_ignis/datagen/loot/ModRecipeProvider.java
@@ -5,6 +5,7 @@ import com.soliid.ab_ignis.block.ModBlocks;
 import com.soliid.ab_ignis.item.ModItems;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.AbstractCookingRecipe;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
@@ -35,9 +36,53 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .unlockedBy(getHasName(ModItems.TIN_INGOT.get()), has(ModItems.TIN_INGOT.get()))
                 .save(pWriter);
 
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.RAW_TIN_BLOCK.get())
+                .pattern("RRR")
+                .pattern("RRR")
+                .pattern("RRR")
+                .define('R',ModItems.RAW_TIN.get())
+                .unlockedBy(getHasName(ModItems.RAW_TIN.get()), has(ModItems.RAW_TIN.get()))
+                .save(pWriter);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.CROOK.get())
+                .pattern("SS")
+                .pattern("S ")
+                .define('S', Items.STICK)
+                .unlockedBy(getHasName(Items.STICK), has(Items.STICK))
+                .save(pWriter);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.FLINT_HATCHET.get())
+                .pattern("SF")
+                .pattern("ST")
+                .define('S', Items.STICK)
+                .define('T', ModItems.TWINE.get())
+                .define('F',Items.FLINT)
+                .unlockedBy(getHasName(Items.FLINT), has(Items.FLINT))
+                .save(pWriter);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.FLINT_KNIFE.get())
+                .pattern("TF")
+                .pattern("S")
+                .define('S', Items.STICK)
+                .define('T', ModItems.TWINE.get())
+                .define('F',Items.FLINT)
+                .unlockedBy(getHasName(Items.STICK), has(Items.STICK))
+                .save(pWriter);
+
         ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.TIN_INGOT.get(), 9)
                 .requires(ModBlocks.TIN_BLOCK.get())
                 .unlockedBy(getHasName(ModBlocks.TIN_BLOCK.get()), has(ModBlocks.TIN_BLOCK.get()))
+                .save(pWriter);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.RAW_TIN.get(), 9)
+                .requires(ModBlocks.RAW_TIN_BLOCK.get())
+                .unlockedBy(getHasName(ModBlocks.RAW_TIN_BLOCK.get()), has(ModBlocks.RAW_TIN_BLOCK.get()))
+                .save(pWriter);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.TWINE.get(), 1)
+                .requires(ModItems.FIBER.get())
+                .requires(ModItems.FIBER.get())
+                .requires(ModItems.FIBER.get())
+                .unlockedBy(getHasName(ModItems.FIBER.get()), has(ModItems.FIBER.get()))
                 .save(pWriter);
     }
 
